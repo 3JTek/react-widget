9a3de0809f7893e573ddaff7e5c046012f950a42
jeremy <jeremy>
2021-08-05 15:23:47 +1000

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Button from 'react-bootstrap/Button';
+import { Col, Container, Image, Row } from 'react-bootstrap';
+
+const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div
+      className='App'
+      style={{ position: 'fixed', bottom: '0', width: '100vw', height: '50vh', backgroundColor: '#e8e8e8', textAlign: 'center' }}
+    >
+      <Container>
+        <Image src={logo} rounded thumbnail width='300' />
+        <Row style={{ margin: '3rem 0' }}>
+          <Col>
+            <Button>Splash it</Button>
+          </Col>
+        </Row>
+        <p>Welcome to SplashUp discovery module</p>
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+          Learn more
         </a>
-      </header>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
